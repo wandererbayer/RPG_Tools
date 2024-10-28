7a6623bb366360181a99a643a200ad20cac86159
wandererbayer <60208886+wandererbayer@users.noreply.github.com>
2024-10-28 12:16:03 -0300

    Upload 1

---
+++ RPG_Tools.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE RPG_tools;
+USE RPG_Tools;
+
+CREATE TABLE Players
+(
+	id_player INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_player VARCHAR(60) NOT NULL,
+    classe_player VARCHAR(60) NOT NULL, 
+    level_player INT NOT NULL,
+    ac_player INT NOT NULL,
+    hp_player INT NOT NULL
+);
+
+CREATE TABLE Enemy
+(
+	id_enemy INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_enemy VARCHAR(60) NOT NULL,
+    ac_enemy INT NOT NULL,
+    hp_enemy INT NOT NULL,
+    xp_enemy INT NOT NULL
+);
+
+SELECT * FROM Players;
+SELECT * FROM Enemy;
